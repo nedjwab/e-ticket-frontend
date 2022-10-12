e569f53145ea45155698d476d34ed52f5738b196
brahimdidi <brahimgb1team@gmail.com>
2022-10-12 18:03:10 +0000

    create event component displaly events

---
+++ src/components/Event.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchEvents, selectEvents } from '../features/events/eventSlice';
+
+export default function Event() {
+  const dispatch = useDispatch();
+  const events = useSelector(selectEvents);
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, []);
+  console.log(events);
+  return (
+    <ul className="list-group p-3">
+      {events.map((game) => (
+        <li key={game.id} className="list-group-item w-25 ">
+          <strong className="text-primary">{game.id}</strong>
+          <h2 className="text-info">{game.name}</h2>
+        </li>
+      ))}
+    </ul>
+  );
+}

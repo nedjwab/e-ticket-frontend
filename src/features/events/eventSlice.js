9a4d905d636ff855b9e0bc4b0c36e9370400796d
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-13 12:01:26 +0100

    Fix errors and clean up files

--- src/features/events/eventSlice.js
+++ src/features/events/eventSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const apiUrl = 'http://localhost:3000/api/v1/events';
+const apiUrl = 'http://127.0.0.1:3000/api/v1/events';
 
 export const fetchEvents = createAsyncThunk('event/fetchevent',
   async () => {

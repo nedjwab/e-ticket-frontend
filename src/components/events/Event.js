0ef7a8223888c8ef89b64e0fe7e240320f1e3a51
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-17 17:12:33 +0100

    Add Event detail component

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchEvents, selectEvents } from '../../features/events/eventSlice';
 
 export default function Event() {
@@ -13,7 +14,7 @@ export default function Event() {
       {events.map((game) => (
         <li key={game.id} className="list-group-item w-25 ">
           <strong className="text-primary">{game.id}</strong>
-          <h2 className="text-info">{game.name}</h2>
+          <Link to={`/events/${game.id}`}>{game.name}</Link>
         </li>
       ))}
     </ul>

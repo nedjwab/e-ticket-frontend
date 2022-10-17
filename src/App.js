056837355edea7fbe5d05fbeaf2072a2c8a41e86
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-17 15:57:57 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Event from './components/Event';
 import MyReservations from './components/Myreservations';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Event />
       <p>Reservations list</p>
       <MyReservations />
     </div>

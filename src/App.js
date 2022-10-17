33797b6d5a7c63e9b283065ac18f33a4887383df
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-10-17 15:38:12 +0100

    Add files for app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Event from './components/events/Event';
 import Navbar from './components/navbar/Navbar';
+import Register from './components/session/Register';
+import SplashScreen from './components/session/SplashScreen';
+import Login from './components/session/Login';
 import MyReservations from './components/reservation/Myreservations';
 import AddReservation from './components/reservation/AddReservation';
 import './App.css';
@@ -9,9 +12,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {(sessionStorage.getItem('loginToken')) ? <Navbar /> : null }
       <Navbar />
       <Routes>
-        <Route path="/" element={<Event />} />
+        <Route exact path="/" element={<SplashScreen />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Event" element={<Event />} />
         <Route path="/Addreservation" element={<AddReservation />} />
         <Route path="/Myreservations" element={<MyReservations />} />
       </Routes>

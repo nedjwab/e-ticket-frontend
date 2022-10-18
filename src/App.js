b0c8f2317b1b0dbfc4dc6b46fa7b5b8f8bcec86f
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 19:22:30 +0100

    Fix detail error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Event from './components/events/Event';
 import Navbar from './components/navbar/Navbar';
 import MyReservations from './components/reservation/Myreservations';
 import AddReservation from './components/reservation/AddReservation';
-import EventDetail from './components/events/EventDetails';
+import EventDetails from './components/events/EventDetails';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Event />} />
         <Route path="/Addreservation" element={<AddReservation />} />
         <Route path="/Myreservations" element={<MyReservations />} />
-        <Route path="/events/:eventID" element={<EventDetail />} />
+        <Route path="/events/:eventID" element={<EventDetails />} />
       </Routes>
     </div>
   );

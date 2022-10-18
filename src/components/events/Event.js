3e03e72d6fa0b0c0dd647929395e569d32c8e307
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 11:39:36 +0100

    Add reserve button

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -15,16 +15,19 @@ export default function Event() {
     <div className="w-100">
       <h1 className="text-center mt-5 mb-2 pt-5 mt-md-1 fw-bold fs-2">Events</h1>
       <h2 className="text-center mt-2 mb-3 fw-bold opacity-50 fs-5">Please select an Event</h2>
-      <div className="card mb-3 details-container">
+      <div className="card-container">
         {events.map((event) => (
           <div key={event.id} className="cards">
-            <img src={event.photo} alt={event.name} />
+            <div className="img-container">
+              <img src={event.photo} alt={event.name} />
+            </div>
             <Link to={`/events/${event.id}`} className="link">{event.name}</Link>
             <div className="card-body">
-              <p className="cancelled-flights contents">
+              <p className="card-text">
                 {' '}
                 {event.description}
               </p>
+              <Link to="/Addreservation" className="link link-decoration">Reserve</Link>
             </div>
           </div>
         ))}

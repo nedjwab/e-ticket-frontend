8a38a1856484c221115a88b2334320a6485031b2
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 12:12:49 +0100

    Add font familly

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -13,15 +13,15 @@ export default function Event() {
 
   return (
     <div className="w-100">
-      <h1 className="text-center mt-5 mb-2 pt-5 mt-md-1 fw-bold fs-2">Events</h1>
-      <h2 className="text-center mt-2 mb-3 fw-bold opacity-50 fs-5">Please select an Event</h2>
+      <h1 className="text">Events</h1>
+      <h2 className="secondry-text">Please select an Event</h2>
       <div className="card-container">
         {events.map((event) => (
           <div key={event.id} className="cards">
             <div className="img-container">
               <img src={event.photo} alt={event.name} />
             </div>
-            <Link to={`/events/${event.id}`} className="link">{event.name}</Link>
+            <Link to={`/events/${event.id}`} className="link card-title">{event.name}</Link>
             <div className="card-body">
               <p className="card-text">
                 {' '}

3c49163139d7c61d4e6aca6875ced3c2c1b64e32
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 11:15:48 +0100

    Add css for the events cards

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -6,17 +6,29 @@ import { fetchEvents, selectEvents } from '../../features/events/eventSlice';
 export default function Event() {
   const dispatch = useDispatch();
   const events = useSelector(selectEvents);
+
   useEffect(() => {
     dispatch(fetchEvents());
   }, []);
+
   return (
-    <ul className="list-group p-3">
-      {events.map((game) => (
-        <li key={game.id} className="list-group-item w-25 ">
-          <strong className="text-primary">{game.id}</strong>
-          <Link to={`/events/${game.id}`}>{game.name}</Link>
-        </li>
-      ))}
-    </ul>
+    <div className="w-100">
+      <h1 className="text-center mt-5 mb-2 pt-5 mt-md-1 fw-bold fs-2">Events</h1>
+      <h2 className="text-center mt-2 mb-3 fw-bold opacity-50 fs-5">Please select an Event</h2>
+      <div className="card mb-3 details-container">
+        {events.map((event) => (
+          <div key={event.id} className="cards">
+            <img src={event.photo} alt={event.name} />
+            <Link to={`/events/${event.id}`} className="link">{event.name}</Link>
+            <div className="card-body">
+              <p className="cancelled-flights contents">
+                {' '}
+                {event.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }

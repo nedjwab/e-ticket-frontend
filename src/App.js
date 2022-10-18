0ab54f7fa07005f8082050a93567e8b333ba6c9d
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 23:59:34 +0100

    Add routes to the app file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './components/navbar/Navbar';
 import MyReservations from './components/reservation/Myreservations';
 import AddReservation from './components/reservation/AddReservation';
 import EventDetails from './components/events/EventDetails';
+import SignUp from './components/users/SignUp';
+import Login from './components/users/Login';
 import './App.css';
 
 function App() {
@@ -13,9 +15,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Event />} />
+        <Route path="/" element={<Login />} />
         <Route path="/Addreservation" element={<AddReservation />} />
         <Route path="/Myreservations" element={<MyReservations />} />
         <Route path="/events/:eventID" element={<EventDetails />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </div>
   );

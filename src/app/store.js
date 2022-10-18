7a5fc9ba98852c499903822fd4775d80d5e5d26f
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-18 23:48:57 +0100

    Add register slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import eventReducer from '../features/events/eventSlice';
 import reservationReducer from '../features/reservations/reservationSlice';
+import registerReducer from '../features/users/registerSlice';
 
 const store = configureStore({
   reducer: {
     event: eventReducer,
     reservation: reservationReducer,
+    userInfo: registerReducer,
   },
 });
 

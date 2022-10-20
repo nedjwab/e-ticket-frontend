0e3181ef9ee31f077bcf5a431cb35e357c8b6d2b
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-20 15:06:57 +0100

    Add addevent form test

--- src/components/__tests__/EvenForm.test.js
+++ src/components/__tests__/EvenForm.test.js
@@ -3,13 +3,13 @@ import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Event from '../events/Event';
+import AddEvent from '../events/EventForm';
 import store from '../../app/store'
 
 describe('Events', () => {
     it('renders correctly', () => {
     const home = renderer
-    .create(<Provider store={store}><Router><Event /></Router></Provider>)
+    .create(<Provider store={store}><Router><AddEvent /></Router></Provider>)
     .toJSON();
   expect(home).toMatchSnapshot();
    });

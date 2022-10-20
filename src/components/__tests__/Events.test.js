57ecaa364979c2abf57f84cf1480029c35b1a40d
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-20 15:04:16 +0100

    Fix events test

--- src/components/__tests__/Events.test.js
+++ src/components/__tests__/Events.test.js
@@ -2,30 +2,16 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Event from '../events/Event';
 import store from '../../app/store'
 
 describe('Events', () => {
-  render(
-    <Provider store={store}>
-    <Router>
-      <Event />
-    </Router>
-    </Provider>,
-  );
-
-  test('The events page header', () => {
-    const header = screen.getByText('Our Events List');
-    expect(header).toBeInTheDocument();
-  });
-
-  test('It renders the Events component correctly', () => {
-    const { container } = render(
-      <Provider store={store}>
-        <Event />
-      </Provider>,
-    );
-    expect(container).toMatchSnapshot();
-  });
+    it('renders correctly', () => {
+    const home = renderer
+    .create(<Provider store={store}><Router><Event /></Router></Provider>)
+    .toJSON();
+  expect(home).toMatchSnapshot();
+   });
 });
